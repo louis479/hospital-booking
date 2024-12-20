7fe4931d5f3a5ab445a6885e6b887e2e71934e27
louis-ngugi <louisngugi858@gmail.com>
2024-12-21 00:50:35 +0300

    created a function to Render a patient details in the list index.js

--- index.js
+++ index.js
@@ -49,4 +49,16 @@ function removePatient(patientId) {
 }
 
 // Render patient details in the list
-function renderPatientList() {}
+function renderPatientList() {
+    patientList.innerHTML = '';
+
+    // Render each patient in the list
+    patients.forEach(patient => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <strong>${patient.name}</strong>, Age: ${patient.age}, Condition: ${patient.condition}, Payment: ${patient.paymentMethod}
+            <button onclick="removePatient(${patient.id})" class="remove-button">Remove</button>
+        `;
+        patientList.appendChild(li);
+    });
+}
