0768554b2131699d78e7c435337c27d43dd42d6d
louis-ngugi <louisngugi858@gmail.com>
2024-12-21 00:30:02 +0300

    created index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// DOM Elements
+const patientForm = document.getElementById('patientForm');
+const patientList = document.getElementById('patientList');
+const searchInput = document.getElementById('searchInput');
+const clearButton = document.getElementById('clearButton');
+
